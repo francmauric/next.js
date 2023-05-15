280fdf7262458d6274656af3e6e57241eae88f37
franc_mauric1 <franc_mauric1@hotmail.com>
2023-05-15 16:47:01 +0200

    first commit

---
+++ app/layout.jsx
@@ -0,0 +1,19 @@
+import "../styles/globals.css"
+
+import { Navigation } from "./components/Navigation";
+
+
+
+export default function RootLayout({ children }) {
+    return (
+      <html lang="en">
+        <head>
+            <title>My first app</title>
+        </head>
+        <body>
+         <Navigation/> 
+            {children}
+        </body>
+      </html>
+    );
+  }
---
+++ app/page.jsx
@@ -0,0 +1,3 @@
+export default function HomePage() {
+    return <h1>Esta es nuestra ap por ahora</h1>;
+  }
